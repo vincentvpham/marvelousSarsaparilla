bc35eda2ca05f791934ed72a432b0de8f47141bc
Robert Boggs <robertstephenboggs@gmail.com>
2016-03-18 14:49:18 -0700

    Add camera component and related routing

--- Pakt/app/components/App.js
+++ Pakt/app/components/App.js
@@ -12,25 +12,26 @@ import SwitchRoute from '../containers/SwitchRoute';
 import Login from './Login';
 import Creator from './Creator';
 import GetPakts from '../containers/GetPakts';
+import Camera from '../components/Camera';
 
-import {Scene, Router, TabBar, Modal, Schema, Actions} from 'react-native-router-flux'
+import { Scene, Router, TabBar, Modal, Schema, Actions } from 'react-native-router-flux';
 
 const scenes = Actions.create(
-  <Scene  key="root">
-    <Scene type="replace" key="login" initial={true} component={Login} title="Login"/>
-    <Scene key="creator" type="replace" component={Creator} title="Create"/>
-    <Scene key="paktList" type="replace" component={GetPakts} title="PaktList"/>
-    {/*<Scene key="camera" type="replace" component={Camera} title="Camera"/>*/}
+  <Scene key="root">
+    <Scene type="replace" key="login" initial={true} component={Login} title="Login" />
+    <Scene key="creator" type="replace" component={Creator} title="Create" />
+    <Scene key="paktList" type="replace" component={GetPakts} title="PaktList" />
+    <Scene key="camera" type="replace" component={Camera} title="Camera" />
   </Scene>
 );
 
 const App = () => (
-  <View style={{flex: 1}}>
-    <Router style={{flex: .4}} scenes={scenes}/>
-    <View style={{flex: .1}}>
-      <SwitchRoute/> 
+  <View style={{ flex: 1 }}>
+    <Router style={{ flex: 0.4 }} scenes={scenes} />
+    <View style={{ flex: 0.1 }}>
+      <SwitchRoute />
     </View>
   </View>
-)
+);
 
 export default App;
