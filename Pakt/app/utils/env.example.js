6ae74f046975ae850498b0d123ca966d15bc08f2
Vincent Pham <vincentvpham@gmail.com>
2016-03-21 12:17:14 -0700

    added dependencies for uploading to amazon S3

---
+++ Pakt/app/utils/env.example.js
@@ -0,0 +1,6 @@
+// insert your credentials here and rename the file 'env.js'
+module.exports = {
+  accessKeyId: 'ENTER_AMAZON_ACCESS_KEY_ID_HERE',
+  secretAccessKey: 'ENTER_AMAZON_SECRET_ACCESS_KEY_HERE',
+  region: 'ENTER_AMAZON_BUCKET_REGION_HERE',
+};
