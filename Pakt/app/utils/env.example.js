f8832a916c2025df5497ab0d22b942c317ee1fdc
Vincent Pham <vincentvpham@gmail.com>
2016-03-21 20:31:21 -0700

    dependencies for uploading to s3 from native camera

---
+++ Pakt/app/utils/env.example.js
@@ -0,0 +1,6 @@
+// insert your credentials here and rename the file 'env.js'
+module.exports = {
+  accessKeyId: 'ENTER_YOUR_AMAZON_KEY_ID_HERE',
+  secretAccessKey: 'ENTER_YOUR_AMAZON_SECRET_ACCESS_KEY_HERE',
+  region: 'ENTER_YOUR_AMAZON_BUCKET_REGION',
+};
