10b6d49a67a954fd25ecb8c26da74cc5542a0c5a
tchamberlain <taylorannchamberlain@gmail.com>
2016-03-21 21:08:05 -0700

    Switch router to attempt to check auth

--- Pakt/app/components/App.js
+++ Pakt/app/components/App.js
@@ -14,26 +14,42 @@ import CreatePakt from '../containers/CreatePakt';
 import GetPakts from '../containers/GetPakts';
 import Camera from '../components/Camera';
 import GetCurrentPakt from '../containers/GetCurrentPakt';
+import { connect } from 'react-redux';
+import { Scene, Router, TabBar, Modal, Schema, Actions, Switch } from 'react-native-router-flux';
 
-import { Scene, Router, TabBar, Modal, Schema, Actions } from 'react-native-router-flux';
+const mapStateToProps = (state) => {
+  return {
+    selector: () => {
+      console.log(state, 'we have a user??');
+      if (!state.users.currentUser) {
+        return 'login';
+      } else {
+        return 'createPakt';
+      }
+    },
+  };
+};
 
-const scenes = Actions.create(
-  <Scene key="root">
-    <Scene type="replace" key="login" initial={true} component={LoginUser} title="Login" />
-    <Scene key="createPakt" type="replace" component={CreatePakt} title="CreatePakt"/>
-    <Scene key="paktList" type="replace" component={GetPakts} title="PaktList" />
-    <Scene key="camera" type="replace" component={Camera} title="Camera" />
-    <Scene key="individualPakt" component={GetCurrentPakt} title="Individual Pakt" />
-  </Scene>
-);
+
 
 const App = () => (
   <View style={{ flex: 1 }}>
-    <Router style={{ flex: 0.4 }} scenes={scenes} />
+    <Router style={{ flex: 0.4 }} scenes={scenes}/>
     <View style={{ flex: 0.1 }}>
       <SwitchRoute />
     </View>
   </View>
+);
+
+const scenes = Actions.create(
+  <Scene key="root"  component={connect(mapStateToProps, null)(Switch)} tabs={true}
+               selector={props=>props.items ? "CreatePakt" : "Login"}>
+    <Scene  key="login" type="replace" component={LoginUser}/>
+    <Scene key="createPakt" type="replace" component={CreatePakt}/>
+    <Scene key="paktList" type="replace" component={GetPakts}/>
+    <Scene key="camera" type="replace" component={Camera}/>
+    <Scene key="individualPakt" component={GetCurrentPakt}/>
+  </Scene>
 );
 
 export default App;
