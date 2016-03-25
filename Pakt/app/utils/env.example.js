73c9b61199d8c7b73cb1917547943943e5f8d719
tchamberlain <taylorannchamberlain@gmail.com>
2016-03-24 17:45:14 -0700

    Add url to env example

--- Pakt/app/utils/env.example.js
+++ Pakt/app/utils/env.example.js
@@ -3,4 +3,5 @@ module.exports = {
   accessKeyId: 'ENTER_YOUR_AMAZON_KEY_ID_HERE',
   secretAccessKey: 'ENTER_YOUR_AMAZON_SECRET_ACCESS_KEY_HERE',
   region: 'ENTER_YOUR_AMAZON_BUCKET_REGION',
+  url: 'http://111.111.111.111:3000/',
 };
