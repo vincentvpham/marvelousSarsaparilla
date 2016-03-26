33b41dca944b7970498b798ef0999547068df583
Robert Boggs <robertstephenboggs@gmail.com>
2016-03-25 21:05:28 -0700

    Update pack list styling

--- Pakt/app/components/PaktListItem.js
+++ Pakt/app/components/PaktListItem.js
@@ -16,30 +16,41 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
+    height: 70,
   },
-  rightContainer: {
-    flex: 1,
+  textContainer: {
+    flex: 0.4,
+  },
+  friendsContainer: {
+    flex: 0.4,
+  },
+  iconContainer: {
+    flex: 0.2,
   },
   name: {
-    fontSize: 20,
-    marginBottom: 8,
+    fontSize: 18,
+    marginBottom: 2,
     textAlign: 'center',
   },
   description: {
+    fontSize: 14,
     textAlign: 'center',
-  },
-  thumbnail: {
-    width: 53,
-    height: 81,
   },
 });
 
 const PaktListItem = ({ pakt, onPaktClick }) => {
   return (
     <View style={styles.container}>
-      <View style={styles.rightContainer}>
+      <View style={styles.textContainer}>
         <Text onPress={() => onPaktClick(pakt)} style={styles.name}>{pakt.name}</Text>
         <Text style={styles.description}>{pakt.description}</Text>
+      </View>
+      <View style={styles.friendsContainer}>
+        <Text>Friendssssssssssss</Text>
+      </View>
+      <View style={styles.iconContainer}>
+        <Image source={{uri: 'https://facebook.github.io/react/img/logo_og.png'}}
+          style={{width: 50, height: 50}} />
       </View>
     </View>
   );
