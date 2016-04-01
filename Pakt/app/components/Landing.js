425a2282f62f2702bf457845f7382279ed6232fa
Vincent Pham <vincentvpham@gmail.com>
2016-04-01 12:15:21 -0700

    animated logo for landing

--- Pakt/app/components/Landing.js
+++ Pakt/app/components/Landing.js
@@ -1,4 +1,5 @@
 import React, {
+  Animated,
   AppRegistry,
   Component,
   Image,
@@ -11,16 +12,54 @@ import React, {
 import LoginUser from '../containers/LoginUser';
 
 const styles = StyleSheet.create({
-  container: {
+  loading: {
+    justifyContent: 'center',
+    position: 'absolute',
+    left: 57,
+  },
+  loadingContainer: {
     flex: 1,
-    justifyContent:'center',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#D0D6D6',
   },
 });
 
-const Landing = ({ title }) => (
-  <View style={styles.container} >
-    <Image source={{uri: 'https://static.pexels.com/photos/2029/landing-stage-sea-holiday-vacation.jpg'}} style={{width: 400, height: 606}}  />
-  </View>
-);
+class Landing extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {fadeAnim: new Animated.Value(0)};
+  }
 
-module.exports = Landing;
+  componentDidMount() {
+    Animated.timing(          
+      this.state.fadeAnim,    
+      {
+        toValue: 1,
+        duration:3000
+      },
+    ).start();                
+  }
+  renderLanding() {
+    return (
+      <View style={styles.loadingContainer} >
+        <Image source={ require('../assets/img/pakt_logo_blank.png') }
+          style={styles.loading}
+        />
+        <Animated.Image source={ require('../assets/img/pakt_logo.png') }
+          style={{opacity:this.state.fadeAnim, position: 'relative', justifyContent: 'center'}}
+        />
+      </View>
+    )
+  }
+  render() {
+    return this.renderLanding();
+  }
+
+
+
+}
+
+
+
+export default Landing;
