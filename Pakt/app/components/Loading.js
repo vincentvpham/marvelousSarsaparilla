e50a6ce9aa0c6ce8a602656555d3758716c925f9
Vincent Pham <vincentvpham@gmail.com>
2016-03-31 22:20:39 -0700

    added loading gif

--- Pakt/app/components/Loading.js
+++ Pakt/app/components/Loading.js
@@ -9,12 +9,25 @@ import React, {
   PropTypes,
 } from 'react-native';
 
+const styles = StyleSheet.create({
+  loadingContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  loading: {
+    width: 150,
+    height: 150,
+    justifyContent: 'center',
+  },
+});
 
-const Loading = ({ displayText = 'Loading Pakts...'}) => (
-  <Text>
-    { displayText } 
-  </Text>
+const Loading = () => (
+  <View style={styles.loadingContainer} >
+    <Image source={ require('../assets/img/loading.gif') }
+      style={styles.loading}
+    />
+  </View>
 );
-
 
 module.exports = Loading;
